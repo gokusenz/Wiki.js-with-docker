d6f3ef1e2ad2fe41e12ecf8a345e3322bc55fdfa
Nattawut Ruangvivattanaroj <nattawut.ru@gmail.com>
2017-04-18 12:43:03 +0700

    add command to Dockerfile

--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-# exec mongodb
-# exec node wiki configure
-node wiki start
-sleep 10000
---
+++ wikijs/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+node wiki start
+for (( ; ; ))
+do
+  #  echo "Pres CTRL+C to stop..."
+   sleep 1
+done
